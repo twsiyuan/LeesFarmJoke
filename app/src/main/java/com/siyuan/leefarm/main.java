0140917f0cfea865135fc2d61a355248ffd37a02
Siyuan Wang <easy0519@gmail.com>
2014-12-06 14:04:44 +0800

    prototype

---
+++ app/src/main/java/com/siyuan/leefarm/main.java
@@ -0,0 +1,57 @@
+package com.siyuan.leefarm;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+
+public class main extends ActionBarActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout);
+	    setupButtons();
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+	void setupButtons(){
+		final Button buttonCow = (Button)findViewById( R.id.button_cow );
+		final Button buttonDog = (Button)findViewById( R.id.button_dog );
+		final Button buttonDuck = (Button)findViewById( R.id.button_duck);
+		final Button buttonAss = (Button)findViewById( R.id.button_ass );
+
+		buttonCow.setOnClickListener( this );
+		buttonDog.setOnClickListener( this );
+		buttonDuck.setOnClickListener( this );
+		buttonAss.setOnClickListener( this );
+	}
+
+	public void onClick(View v) {
+		final int resID = getMediaResIdByButtonID( v.getId() );
+
+		MediaPlayer mediaPlayer = MediaPlayer.create(this,resID);
+		mediaPlayer.start();
+	}
+
+	int getMediaResIdByButtonID( final int buttonID ){
+		switch ( buttonID ){
+			case R.id.button_cow:   return R.raw.cow;
+			case R.id.button_dog:   return R.raw.dog;
+			case R.id.button_duck:  return R.raw.duck;
+			case R.id.button_ass:   return R.raw.ass;
+		}
+		throw new IllegalArgumentException( "buttonID is not valid" );
+	}
+
+}
